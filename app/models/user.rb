5aef46d4b5f3cfccad9c4dbc75e215c772381510
Scott Hale <shalemont@gmail.com>
2013-11-27 14:13:45 -0700

    artist yn fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :first_name, :last_name, :city, :state, :art_website, 
                   :type_of_artist, :artist_statement, :email, :password, :password_confirmation, :remember_me,
-                  :provider, :uid
+                  :provider, :uid, :artist_yn
 
 # if an artist (radio button- yes), then:
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  ## validates :art_website,'url is valid', presence: true
-  # validates :type_of_artist, presence: true, length: {maximum: 40} 
+  validates :first_name, presence: true, if artist_yn = true
+  validates :last_name, presence: true
+  validates :art_website,'url is valid', presence: true
+  validates :type_of_artist, presence: true, length: {maximum: 40} 
   # validates :artist_statement, length: { maximum: 500 }, presence: true
   ## validates :artist_yn, ---- one or other button has been selected
   
