df256fb46ab3e784eadc7663bf5f898542b650cd
Scott Hale <shalemont@gmail.com>
2013-12-04 18:35:48 -0700

    changing artwork index

--- app/uploaders/art_image_uploader.rb
+++ app/uploaders/art_image_uploader.rb
@@ -27,11 +27,14 @@ class ArtImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fill => [200, 200]
+  process :resize_to_fill => [500, 500]
   #
   # def scale(width, height)
   #   # do something
   # end
+  version :small do
+    process :resize_to_fill => [180, 180]
+  end
 
   # Create different versions of your uploaded files:
   version :thumb do
