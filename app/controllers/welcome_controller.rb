9ad3742646b7ee14701ac97601cb6cb719a3d9d2
Scott Hale <shalemont@gmail.com>
2013-12-10 21:58:24 -0700

    tuesday night from laptop 12/10

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
 
   # have conditionally render based on whether is_an_artist at login
-  def index
-    redirect_to welcome_dashboard_path if current_user.is_an_artist
-  end
+  # def index
+  #   redirect_to welcome_dashboard_path if current_user.is_an_artist
+  # end
 
   def about
   end
