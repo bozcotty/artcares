91fa3c60a813c585e76d1dff9486874cd76bea1d
Scott Hale <user@Scotts-MacBook-Pro.local>
2013-12-24 16:12:39 -0700

    remove unnecessary migration ships from zip

--- db/migrate/20131130004857_remove_ships_from_zip_from_artworks.rb
+++
@@ -1,9 +0,0 @@
-class RemoveShipsFromZipFromArtworks < ActiveRecord::Migration
-  def up
-    remove_column :artworks, :ships_from_zip
-  end
-
-  def down
-    add_column :artworks, :ships_from_zip, :integer
-  end
-end
