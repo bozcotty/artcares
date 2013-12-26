e9292c2e96c90f9c7fc74bd62ebacfd016ac1a85
Scott Hale <user@Scotts-MacBook-Pro.local>
2013-12-25 17:07:44 -0700

    seeded user scotthale12

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+u = User.new(first_name: 'Scott', last_name: 'Hale', email: 'scotthale12@gmail.com', password: 'kayaks10', password_confirmation: 'kayaks10')
+u.skip_confirmation!
+u.save
+u.update_attribute(:artist_yn, 'true')
