aff63916f3f38538dedb269223844764eee403c7
Scott Hale <user@Scotts-MacBook-Pro.local>
2013-12-30 19:02:04 -0700

    add keys for stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,7 @@
 Rails.configuration.stripe = {
-  # :publishable_key => ENV['PUBLISHABLE_KEY'],
-  # :secret_key      => ENV['SECRET_KEY']
-  #:publishable_key => pk_foo,
-  #:secret_key => sk_bar
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :secret_key      => ENV['STRIPE_SECRET_KEY']
+
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
