bda714540bfa95cdb9d7d6ff798e32eaf5fbd2de
Scott <Scott@Scotts-MacBook-Pro.local>
2014-04-03 23:15:38 -0600

    bootstrap navbar

--- db/migrate/20140402172652_add_patient_contact_info_to_patient_campaigns.rb
+++ db/migrate/20140402172652_add_patient_contact_info_to_patient_campaigns.rb
@@ -4,5 +4,7 @@ class AddPatientContactInfoToPatientCampaigns < ActiveRecord::Migration
     add_column :patient_campaigns, :patient_unit_number, :string
     add_column :patient_campaigns, :patient_zip_code, :string
     add_column :patient_campaigns, :patient_phone_number, :string
+    
   end
 end
+
