afa6b746641c46902082a8720cfe9a9e406b625a
Scott <Scott@Scotts-MacBook-Pro.local>
2014-04-15 22:22:13 -0600

    add patient address inputs to patient_campaign model accessible

--- app/models/patient_campaign.rb
+++ app/models/patient_campaign.rb
@@ -1,6 +1,6 @@
 class PatientCampaign < ActiveRecord::Base
   attr_accessible :campaign_name, :artist_relationship_to_patient, :patient_city, :patient_diagnosis, :patient_first_name, :patient_last_name, :patient_state, 
-                  :patient_story, :patient_image_1
+                  :patient_story, :patient_image_1, :patient_street_address, :patient_unit_number, :patient_zip_code, :patient_phone_number
 
   belongs_to :user
   has_many :artworks
