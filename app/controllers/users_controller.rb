d9501e5a1a77340af0bc350631a4e3ae6505b16b
Scott <Scott@Scotts-MacBook-Pro.local>
2014-05-07 09:31:41 -0600

    user#show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,12 @@ class UsersController < ApplicationController
 # for showing the user his own list of artworks and patient campaigns privately
 
   def index
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+    @patient_campaign = @user.patient_campaign
   end
 
 end
