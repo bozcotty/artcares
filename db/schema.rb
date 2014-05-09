9609346311fca0d5a2b1ebaa9f3010c6e4f84574
Scott <Scott@Scotts-MacBook-Pro.local>
2014-05-09 12:40:16 -0600

    change name of migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140507202744) do
+ActiveRecord::Schema.define(:version => 20140509162408) do
 
   create_table "artworks", :force => true do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20140507202744) do
     t.integer  "user_id"
     t.integer  "patient_campaign_id"
     t.boolean  "original_work"
+    t.string   "category"
   end
 
   create_table "buyers", :force => true do |t|
@@ -108,6 +109,9 @@ ActiveRecord::Schema.define(:version => 20140507202744) do
     t.string   "unit_number"
     t.string   "phone_number"
     t.string   "zip_code"
+    t.string   "sample_work_1"
+    t.string   "sample_work_2"
+    t.string   "sample_work_3"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
