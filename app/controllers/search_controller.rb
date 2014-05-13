565083696eaae21673daa3abb9c3182471fc0795
Scott <Scott@Scotts-MacBook-Pro.local>
2014-05-13 15:04:59 -0600

    more pg_search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,11 @@
 class SearchController < ApplicationController
   def index
-    @search = Sunspot.search(PatientCampaign, Artwork, User) do
+    include PgSearch
+    @search = PgSearch.multisearch(PatientCampaign, Artwork, User) do
       fulltext params[:search]
       paginate(page: params[:page], per_page: 5)
     end
     @results = @search.results
   end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140513172323) do
+ActiveRecord::Schema.define(:version => 20140513200731) do
 
   create_table "artworks", :force => true do |t|
     t.string   "title"
