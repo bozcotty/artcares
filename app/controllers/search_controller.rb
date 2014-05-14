36bb507d1c1beb7495d2ec3d508ae686d0e78e3b
Scott <Scott@Scotts-MacBook-Pro.local>
2014-05-14 14:22:50 -0600

    start mods to user#show, p_c#show to add artworks

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    include PgSearch
+    # include PgSearch
     @search = PgSearch.multisearch(PatientCampaign, Artwork, User) do
       fulltext params[:search]
       paginate(page: params[:page], per_page: 5)
