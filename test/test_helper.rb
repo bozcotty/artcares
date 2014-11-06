75439b9e32b51ece40ec975f65529b3bbc946a86
Scott Hale <shalemont@gmail.com>
2014-11-05 18:46:06 -0800

    create "create_campaign" method in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,3 +22,25 @@ def sign_in(fixture_name)
   fill_in 'Password', with: '12345678'
   click_on 'Sign in'
 end
+
+def create_campaign
+  visit welcome_dashboard
+
+  fill_in "Name your Patient Campaign, (i.e. 'Art for Mary\'s Cancer Fund'", with:
+
+end
+
+def create_artwork
+  visit new_campaign_artwork_path
+
+  fill_in 'Title of Artwork', with: artworks(:one).title
+  fill_in 'Dimensions/Size', with: artworks(:one).size
+  check('Verify this is an original Artwork by checking the box.')
+  select('sculpture', :from => 'Art Category')
+  fill_in 'Medium (i.e. oil, acrylic, watercolor. etc.)', with: artworks(:one).medium
+  fill_in 'Price (US $)', with: artworks(:one).price
+  fill_in 'Shipping & Handling Cost (US $)', with: artworks(:one).shipping_price
+  click_on 'List Artwork'
+end
+
+
